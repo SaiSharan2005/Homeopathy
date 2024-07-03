a560360e0236957165d57057956f68795756ed9d
SaiSharan2005 <duginisaisharan@gmail.com>
2024-07-03 13:43:44 +0530

    fixed error in booking appointment

--- hospital/src/main/java/com/G19/hospital/repository/BookingAppointmentRepository.java
+++ hospital/src/main/java/com/G19/hospital/repository/BookingAppointmentRepository.java
@@ -9,12 +9,11 @@ import com.G19.hospital.model.Authentication.PatientRegister;
 import java.util.List;
 
 public interface BookingAppointmentRepository extends JpaRepository<BookingAppointment, Long> {
-       List<BookingAppointment> findByDoctorId(DoctorRegister doctorId);
-       BookingAppointment findByBookingId(Long bookingId);
+    List<BookingAppointment> findByDoctorId(DoctorRegister doctorId);
+    
+    BookingAppointment findByBookingId(Long bookingId);
+    List<BookingAppointment> findByScheduleId(DoctorSchedule schedule);
+    BookingAppointment findByToken(String token);
     List<BookingAppointment> findByPatientId(PatientRegister patientId);
 
-    List<BookingAppointment> findByScheduleId(DoctorSchedule scheduleId);
-
-    BookingAppointment findByToken(String token);
 }
-
