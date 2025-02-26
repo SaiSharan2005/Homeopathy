d5e2560ea9af26d1bebdfdb3853a6fb4ecf53937
SaiSharan2005 <duginisaisharan@gmail.com>
2025-02-26 07:08:39 +0530

    add codes

--- src/main/java/com/G19/hospital/model/inventory/Category.java
+++ src/main/java/com/G19/hospital/model/inventory/Category.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.G19.hospital.model.User;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Getter
 @Setter
--- src/main/java/com/G19/hospital/model/inventory/InventoryItem.java
+++ src/main/java/com/G19/hospital/model/inventory/InventoryItem.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.G19.hospital.model.User;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Getter
 @Setter
