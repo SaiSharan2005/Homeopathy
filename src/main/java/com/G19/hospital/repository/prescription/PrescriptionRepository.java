e0ae52c1cfc810bd1dd1bc5b7107d9555d14ee6a
Sai Sharan <108337097+SaiSharan2005@users.noreply.github.com>
2025-03-09 07:36:44 +0530

    Update PrescriptionRepository.java

--- src/main/java/com/G19/hospital/repository/prescription/PrescriptionRepository.java
+++ src/main/java/com/G19/hospital/repository/prescription/PrescriptionRepository.java
@@ -3,7 +3,9 @@ package  com.G19.hospital.repository.prescription;
 import com.G19.hospital.model.prescription.Prescription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
+import java.util.Optional;
+ 
 @Repository
 public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
     // Add custom query methods if needed
